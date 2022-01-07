ea1ae14135db4c4b055e2f2e025da6a847ec6616
Artem Mezentsev <mezentsv@gmail.com>
2022-01-07 20:06:48 +0300

    feat(markdown): add markdown editor

---
+++ src/components/text-editor.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect, useRef } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+
+import './text-editor.css';
+
+const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('Hello markdown');
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+    return () =>
+      document.removeEventListener('click', listener, { capture: true });
+  }, []);
+
+  if (editing) {
+    return (
+      <div className="text-editor" ref={ref}>
+        <MDEditor
+          value={value}
+          onChange={(newValue = '') => setValue(newValue)}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
+    </div>
+  );
+};
+
+export default TextEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import CodeUnit from './components/code-unit';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
     <div>
-      <CodeUnit />
+      <TextEditor />
+      {/* <CodeUnit /> */}
     </div>
   );
 };
