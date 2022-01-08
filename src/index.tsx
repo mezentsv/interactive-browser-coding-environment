3dd7b405af8728080daa20d729d5e0d434205f68
Artem Mezentsev <mezentsv@gmail.com>
2022-01-08 22:22:48 +0300

    feat: scaffold redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './state';
 
 import CodeUnit from './components/code-unit';
+import TextEditor from './components/text-editor';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-      {/* <CodeUnit /> */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+        {/* <CodeUnit /> */}
+      </div>
+    </Provider>
   );
 };
 
