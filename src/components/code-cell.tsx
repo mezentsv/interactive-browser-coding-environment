bb41b11326e9cc8145d5ad24d63ee7b165d0ee94
Artem Mezentsev <mezentsv@gmail.com>
2022-01-15 20:23:50 +0300

    feat(code_editor): add cumulative code exec feature

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import { useCumulativeCode } from '../hooks/use-cumulative-code';
 import CodeEditor from './code-editor';
 import Resizable from './resizable';
 import Preview from './preview';
@@ -16,22 +17,23 @@ type CodeCellProps = {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  const cumulativeCode = useCumulativeCode(cell.id);
   const { id, content } = cell;
 
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
       return;
     }
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode);
     }, 1000);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cell.content, cell.id, createBundle]);
+  }, [cumulativeCode, cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
